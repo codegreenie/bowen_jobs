595ed23e7b3ee65eda52a7cc5f1a6a4b722e266c
codegreenie <codegreenie@gmail.com>
2017-12-12 09:33:55 +0100

    Add files via upload

--- www/js/fpews.js
+++ www/js/fpews.js
@@ -45,12 +45,14 @@ myApp.onPageInit('startPage', function(page){
 			        {
 			            text: 'Student',
 			            bold: true,
-			            color : 'blue'
+			            color : 'blue',
+			            onClick : function(){mainView.router.loadPage('studentlogin.html')}
 			        },
 			        {
 			            text: 'Staff',
 			            bold : true,
-			            color : 'blue'
+			            color : 'blue',
+			            onClick : function(){mainView.router.loadPage('stafflogin.html')}
 			        },
 			        {
 			            text: 'Admin',
@@ -107,7 +109,7 @@ myApp.onPageInit('adminlogin', function(page){
 	});
 
 
-	$$('form.ajax-submit').on('form:success', function (e) {
+	$$('form.admin-login').on('form:success', function (e) {
 					 
 		 var xhr = e.detail.xhr; // actual XHR object
 
@@ -236,3 +238,345 @@ myApp.onPageInit('allstaffs', function(page){
 
 
 });
+
+
+
+
+
+var deleteJob, approveJob;
+
+myApp.onPageInit('pendingjobs', function(page){
+
+			approveJob = function(jobSN){
+
+				$$.post("http://tmlng.com/Mobile_app_repo/php_hub/_Fpe_Work_Study/admin_job_approver.php", {'job_identity' : jobSN}, function(data){
+
+					if(data == "Approved!"){
+
+						mainView.router.reloadPage("pendingjobs.html");
+					}
+					else{
+
+						myApp.alert("Approval error occured. Try later");
+					}
+
+
+				}, function(){
+
+					myApp.alert("Network Error, Try again.");
+
+				});
+			}
+
+
+			deleteJob = function(jobSN){
+
+
+				myApp.alert("Wayoooo!");
+			}
+
+
+
+
+			$$.get("http://tmlng.com/Mobile_app_repo/php_hub/_Fpe_Work_Study/load_pending_jobs.php", function(data){
+
+						$$(".load-pending-jobs").html(data);
+				}, function(){
+
+					myApp.alert("Network Error, Try again.");
+
+				});
+
+
+			
+
+});
+
+
+
+
+myApp.onPageInit('addstudent', function(page){
+
+
+	$$("#add-new-student-btn").on('click', function(e){
+
+			$$('form.add-new-student').trigger('submit');
+
+	});
+
+
+	$$('form.add-new-student').on('form:beforesend', function (e) {
+					  
+					  myApp.showPreloader(' ');
+	});
+
+
+	$$('form.add-new-student').on('form:error', function (e) {
+					  
+						myApp.hidePreloader();
+						myApp.alert("Network Error, Try again later");
+
+	});
+
+
+	$$('form.add-new-student').on('form:success', function (e) {
+					 
+		 var xhr = e.detail.xhr; // actual XHR object
+
+		 var data = e.detail.data; // Ajax response from action file
+		 
+		 myApp.hidePreloader();
+		 if(data == "Done"){
+
+		 	mainView.router.loadPage("allstudents.html");
+		 }
+
+		 else{
+
+		 	myApp.alert(data);
+		 }
+
+		 console.log(data);
+		 
+
+	});
+
+
+});
+
+
+
+
+
+
+
+
+
+
+myApp.onPageInit('addstaff', function(page){
+
+
+	$$("#add-new-staff-btn").on('click', function(e){
+
+			$$('form.add-new-staff').trigger('submit');
+
+	});
+
+
+	$$('form.add-new-staff').on('form:beforesend', function (e) {
+					  
+					  myApp.showPreloader(' ');
+	});
+
+
+	$$('form.add-new-staff').on('form:error', function (e) {
+					  
+						myApp.hidePreloader();
+						myApp.alert("Network Error, Try again later");
+
+	});
+
+
+	$$('form.add-new-staff').on('form:success', function (e) {
+					 
+		 var xhr = e.detail.xhr; // actual XHR object
+
+		 var data = e.detail.data; // Ajax response from action file
+		 
+		 myApp.hidePreloader();
+		 if(data == "Done"){
+
+		 	mainView.router.loadPage("allstaffs.html");
+		 }
+
+		 else{
+
+		 	myApp.alert(data);
+		 }
+
+		 console.log(data);
+		 
+
+	});
+
+
+});
+
+
+
+
+
+
+
+myApp.onPageInit('stafflogin', function(page){
+
+
+	$$("#staff-login-btn").on('click', function(e){
+
+					$$('form.staff-login').trigger('submit');
+
+	});
+
+
+	$$('form.staff-login').on('form:beforesend', function (e) {
+					  
+					  myApp.showPreloader(' ');
+	});
+
+
+	$$('form.staff-login').on('form:error', function (e) {
+					  
+						myApp.hidePreloader();
+						myApp.alert("Network Error, Try again later");
+
+	});
+
+
+	$$('form.staff-login').on('form:success', function (e) {
+					 
+		 var xhr = e.detail.xhr; // actual XHR object
+
+		 var datas = e.detail.data; // Ajax response from action file
+		 var datas2 = JSON.parse(datas);
+		 myApp.hidePreloader();
+
+		 console.log(datas2.login_status);
+		 if(datas2.login_status == "Invalid Login"){
+
+		 	myApp.alert("Invalid Login");
+		 }
+		 else if(datas2.login_status == "Please Complete the form!"){
+
+		 	myApp.alert("Please Complete the form!");
+
+		 }
+		 else{
+
+		 	window.localStorage.setItem("Staff_SN", datas2.staff_sn);
+		 	mainView.router.loadPage("staffdashboard.html");
+		 }
+
+	});
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+myApp.onPageInit('staffdashboard', function(page){
+
+			
+var staffSN = window.localStorage.getItem("Staff_SN");
+
+			$$.get("http://tmlng.com/Mobile_app_repo/php_hub/_Fpe_Work_Study/staff_jobs_puller.php", {'staff_sn' : staffSN},function(data){
+
+						$$(".load-staff-jobs").html(data);
+				}, function(){
+
+					myApp.alert("Network Error, Try again.");
+
+				});
+
+
+});
+
+
+
+
+
+myApp.onPageInit('studentdashboard', function(page){
+
+			
+var staffSN = window.localStorage.getItem("Staff_SN");
+
+			$$.get("http://tmlng.com/Mobile_app_repo/php_hub/_Fpe_Work_Study/student_job_puller.php", function(data){
+
+						$$(".load-student-jobs").html(data);
+				}, function(){
+
+					myApp.alert("Network Error, Try again.");
+
+				});
+
+
+});
+
+
+
+
+
+
+
+
+
+myApp.onPageInit('studentlogin', function(page){
+
+
+	$$("#student-login-btn").on('click', function(e){
+
+					$$('form.student-login').trigger('submit');
+
+	});
+
+
+	$$('form.student-login').on('form:beforesend', function (e) {
+					  
+					  myApp.showPreloader(' ');
+	});
+
+
+	$$('form.student-login').on('form:error', function (e) {
+					  
+						myApp.hidePreloader();
+						myApp.alert("Network Error, Try again later");
+
+	});
+
+
+	$$('form.student-login').on('form:success', function (e) {
+					 
+		 var xhr = e.detail.xhr; // actual XHR object
+
+		 var datas = e.detail.data; // Ajax response from action file
+		 var datas2 = JSON.parse(datas);
+		 myApp.hidePreloader();
+
+		 console.log(datas2.login_status);
+		 if(datas2.login_status == "Invalid Login"){
+
+		 	myApp.alert("Invalid Login");
+		 }
+		 else if(datas2.login_status == "Please Complete the form!"){
+
+		 	myApp.alert("Please Complete the form!");
+
+		 }
+		 else{
+
+		 	window.localStorage.setItem("student_SN", datas2.student_sn);
+		 	mainView.router.loadPage("studentdashboard.html");
+		 }
+
+	});
+
+
+
+
+
+});
+
